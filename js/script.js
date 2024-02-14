345bebed86657bb6241d76fd65a338adbb757f97
ViolaLast <153906000+ViolaLast@users.noreply.github.com>
2024-02-14 11:02:09 +0000

    js adjustments attempts

--- js/script.js
+++ js/script.js
@@ -1,89 +1,83 @@
-var currentFetchedImageUrl = '';
+$(document).ready(function () {
+    var assignedImages = [];
+    var currentImageUrl; // Variable to store the current image URL
 
-// Event listener for finding and displaying a new image
-$('#findImageBtn').click(function () {
-    fetchAndDisplayImage();
-});
+    // Event listener for finding and displaying a new image
+    $('#findImageBtn').click(function () {
+        fetchAndDisplayImage();
+    });
 
-// Event listener for adding email to the dropdown
-$('#assignImageBtn').click(function () {
-    var email = $('#emailInput').val();
-    if (validateEmail(email)) {
-        assignImageToEmail(email);
-    } else {
-        alert('Please enter a valid email address.');
+    // Event listener for adding email to the dropdown
+    $('#assignImageBtn').click(function () {
+        var email = $('#emailInput').val();
+        if (validateEmail(email)) {
+            addToDropdown(email);
+        } else {
+            alert('Please enter a valid email address.');
+        }
+    });
+
+    // Event listener for adding the current image to the imgDisplayer
+    $('#btnAddImage').click(function () {
+        var selectedEmail = $('#emailDropdown').val();
+        if (selectedEmail !== 'Please select your email') {
+            assignImageToEmail(selectedEmail);
+            // Fetch and display a new image in imageWindow
+            fetchAndDisplayImage();
+        } else {
+            alert('Please select an email first.');
+        }
+    });
+
+    // Function to validate email
+    function validateEmail(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
     }
-});
 
-// Function to validate email
-function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-
-// Function to assign email to dropdown
-function assignImageToEmail(email) {
-    // Add the validated email to the selection list
-    var $emailDropdown = $('#emailDropdown');
-    var $option = $('<option>').text(email).val(email);
-    $emailDropdown.append($option);
-
-    // Optional: Select the newly added email for convenience
-    $emailDropdown.val(email);
-
-    // Clear the input field for the next email
-    $('#emailInput').val('');
-}
-
-// Event listener for adding the current image to the imgDisplayer
-$('#btnAddImage').click(function () {
-    var selectedEmail = $('#emailDropdown').val();
-    if (selectedEmail !== 'Please select your email') {
-        addImageToDisplay(selectedEmail, currentFetchedImageUrl); // Pass the current fetched image URL
-    } else {
-        alert('Please select an email first.');
+    // Function to assign email to dropdown and display image
+    function assignImageToEmail(email) {
+        var $imgDisplay = $('#imgDisplay');
+        var currentImage = currentImageUrl; // Use the stored URL
+        if (currentImage) {
+            var $image = $('<img>').attr('src', currentImage).addClass('displayed-image');
+            $imgDisplay.append($('<p>').text('Selected Email: ' + email), $image);
+            assignedImages.push({ email: email, imageUrl: currentImage });
+        } else {
+            alert('No image available. Please find and assign an image first.');
+        }
     }
-});
 
-function addImageToDisplay(selectedEmail, imageUrl) {
-    var $imgDisplay = $('#imgDisplay');
-    var $imageWindow = $('#imageWindow');
+    // Function to add email to dropdown
+    function addToDropdown(email) {
+        var $emailDropdown = $('#emailDropdown');
+        var $option = $('<option>').text(email).val(email);
+        $emailDropdown.append($option);
+        $emailDropdown.val(email);
+        $('#emailInput').val('');
+    }
 
-    if (imageUrl) {
-        var $image = $('<img>').attr('src', imageUrl).addClass('displayed-image');
-        $imgDisplay.append($('<p>').text('Selected Email: ' + selectedEmail), $image);
+    // Function to fetch and display an image
+    function fetchAndDisplayImage() {
+        var image = new Image();
+        image.onload = function () {
+            currentImageUrl = image.src; // Store the current image URL
+            displayImageInWindow(currentImageUrl);
+        };
+        image.onerror = function () {
+            console.error('Error loading image.');
+        };
+        image.src = 'https://source.unsplash.com/random';
+    }
 
-        // Clear the image in imageWindow
+    // Function to display an image in the imageWindow
+    function displayImageInWindow(imageUrl) {
+        var $imageWindow = $('#imageWindow');
         $imageWindow.empty();
-    } else {
-        alert('No image available. Please find and assign an image first.');
-        return; // Stop execution if there's no image available
+        var $image = $('<img>').attr('src', imageUrl).addClass('fetched-image');
+        $image.appendTo($imageWindow);
     }
 
-    // Fetch and display a new image in imageWindow
+    // Fetch and display a new image on the load of the page
     fetchAndDisplayImage();
-}
-
-// Fetch and display a new image on the load of the page
-fetchAndDisplayImage();
-
-// Function to fetch and display an image
-function fetchAndDisplayImage() {
-    var image = new Image();
-    image.onload = function () {
-        currentFetchedImageUrl = image.src;
-        displayImageInWindow(currentFetchedImageUrl);
-    };
-    image.onerror = function () {
-        console.error('Error loading image.');
-    };
-    image.src = 'https://source.unsplash.com/random';
-}
-
-// Function to display an image in the imageWindow
-function displayImageInWindow(imageUrl) {
-    var $imageWindow = $('#imageWindow');
-    $imageWindow.empty();
-    var $image = $('<img>').attr('src', imageUrl).addClass('fetched-image');
-    $image.appendTo($imageWindow);
-}
+});
